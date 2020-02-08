17ce94d803768ae93010794c0fc6b6d191c11be4
shahZ51 <shahriyar.ahmed@gmail.com>
2020-02-08 15:01:44 -0500

    modified my js file

--- webapp/static/js/lg_elcompare2.js
+++ webapp/static/js/lg_elcompare2.js
@@ -3,7 +3,7 @@ function renderComparison2(objReportData1, objReportData2){
    //console.log(objBrexit);
    //console.log(objNatlElec);
    console.log(objReportData1);
-   console.log(objReportData2);
+   //console.log(objReportData2);
    
 }
 
