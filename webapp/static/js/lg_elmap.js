66ef2cd06b6a447a563ea756770a82c6447d4a98
shahZ51 <shahriyar.ahmed@gmail.com>
2020-02-08 12:18:53 -0500

    recommited

--- webapp/static/js/lg_elmap.js
+++ webapp/static/js/lg_elmap.js
@@ -2,7 +2,7 @@ function renderMap(objReportData){
    // console.log("Render Map " + reportElemID);       
     //console.log(reportData);
    // console.log(dataRegions);
-   console.log("Made my mapping");
+   
     
     
 }
