5e17e5c6b5211b067db468774cca2490859c22c7
colmenareso <colmenareso@users.noreply.github.com>
2020-02-08 14:43:21 -0500

    New change

--- webapp/static/js/lg_elcompare2.js
+++ webapp/static/js/lg_elcompare2.js
@@ -4,6 +4,7 @@ function renderComparison2(objReportData1, objReportData2){
    //console.log(objNatlElec);
    console.log(objReportData1);
    console.log(objReportData2);
+   console.log("Hello");
    
 }
 
